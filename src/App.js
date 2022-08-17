9800c15bcdd25bb98db6ff603de11ec5128dcbc0
Mahamadou Kabba <mkabba34@gmail.com>
2022-08-16 20:55:00 -0700

    components created

--- src/App.js
+++ src/App.js
@@ -1,19 +1,26 @@
-import logo from "./logo.svg";
 import "./App.css";
 import Nav from "./pages/nav/Nav";
-import { Routes } from "react-router-dom";
+import About from "./pages/about/About"
+import Work from "./pages/work/Work"
+import Resume from "./pages/resume/Resume"
+import { Route, Routes } from "react-router-dom";
+import Contact from "./pages/contact/Contact";
+
+// import { Routes } from "react-router-dom";
 
 function App() {
   return (
     <body>
       <Nav>
-        {/* <Routes>
-           <Route  />
-           <Route  />
+        <Routes>
+        <Route path="/" element={<About />} />
+           <Route  path="work" element={<Work/>}/>
+           <Route path="resume" element={<Resume/>} />
+           <Route path="contact" element={<Contact/>} />
            <Route  />
 
            
-        </Routes> */}
+        </Routes>
       </Nav>
     </body>
   );
