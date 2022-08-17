4c9f01413b8c1eacdabcfb8630126b901bbcda87
Mahamadou Kabba <mkabba34@gmail.com>
2022-08-17 14:26:06 -0700

    about page up and running

--- src/pages/about/About.js
+++ src/pages/about/About.js
@@ -1,8 +1,33 @@
 import React from 'react'
+import "./About.css"
 
 function About() {
   return (
-    <div>About</div>
+    <div className='main-about'> 
+    {/* this stay to the left */}
+    <h1 className='myname'> MAHAMADOU KABBA</h1>
+      
+    <div className='about-text'>
+    
+      <h1 className='frontend'>
+        Frontend, Backend Developer and UX
+      </h1>
+      <br></br>
+      
+      <div className='about-p'>
+      <p className='about-p1'>
+        Hello, I'm Mahamdou. Nice to meet you.
+      </p>
+      <p> I am a Web / Software Designer based in Bronx, NY.</p>
+      <p> Currently I'm working at Cognizant as a contractor.</p>
+      <p>  Open to new opportunities.</p>
+      
+      
+      </div>
+
+    
+    </div>
+    </div>
   )
 }
 
