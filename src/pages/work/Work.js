bb0abafe8f4226a11657c03a44411e4266493032
Mahamadou kabba <kabba-16@jenosakl1grmd6p.myfiosgateway.com>
2022-08-20 10:26:49 -0400

    iframe display

--- src/pages/work/Work.js
+++ src/pages/work/Work.js
@@ -1,11 +1,32 @@
-import React from 'react'
+import React from "react";
+import "./Work.css";
 
 function Work() {
   return (
-    <div>Work
-        work goes here
-    </div>
-  )
+    <>
+      <br></br>
+      <div className="main-work">
+        <div>
+          <iframe
+            src="https://jpmare29.github.io/project-1-group-8-columbia/"
+            className="iframe2"
+          ></iframe>
+          <iframe
+            src="https://moekabba.github.io/portfolio-generator/"
+            className="iframe2"
+          ></iframe>
+          <iframe
+            src="https://moekabba.github.io/weather-dasboard/"
+            className="iframe2"
+          ></iframe>
+          <iframe
+            src=" https://moekabba.github.io/code-quiz/"
+            className="iframe2"
+          ></iframe>
+        </div>
+      </div>
+    </>
+  );
 }
 
-export default Work
+export default Work;
