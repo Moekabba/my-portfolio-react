c296d1ee32e9d38be70171aee1cedf2d8c71529e
Mahamadou Kabba <mkabba34@gmail.com>
2022-08-28 22:53:46 -0400

    added work

--- src/pages/work/Work.js
+++ src/pages/work/Work.js
@@ -1,11 +1,40 @@
-import React from 'react'
+import React from "react";
+import "./Work.css";
 
 function Work() {
   return (
-    <div>Work
-        work goes here
+    <div class="d-flex flex-column" id="work-id">
+      <div class="p-2">
+        <iframe
+          src="https://jpmare29.github.io/project-1-group-8-columbia/"
+          style={{ width: "200%", height: "47rem" }}
+        ></iframe>
+      </div>
+      <br></br>
+      <div class="p-2">
+        <iframe
+          src="https://moekabba.github.io/weather-dasboard/"
+          style={{ width: "200%", height: "45rem" }}
+        ></iframe>
+      </div>
+      <br></br>
+
+      <div class="p-2">
+        <iframe
+          src="https://moekabba.github.io/portfolio-generator/"
+          style={{ width: "200%", height: "45rem" }}
+        ></iframe>
+      </div>
+      <br></br>
+
+      <div>
+        <iframe
+          src=" https://moekabba.github.io/code-quiz/"
+          style={{ width: "100%", height: "45rem" }}
+        ></iframe>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Work
+export default Work;
