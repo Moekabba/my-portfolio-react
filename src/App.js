f4ed7462b091b1e6c7a3eaeec7a94dfca88db0c7
Mahamadou Kabba <mkabba34@gmail.com>
2023-02-25 12:55:05 -0500

    pull

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
 import "./App.css";
 import Nav from "./pages/nav/Nav";
-import About from "./pages/about/About"
-import Work from "./pages/work/Work"
-import Resume from "./pages/resume/Resume"
+import About from "./pages/about/About";
+import Work from "./pages/work/Work";
+import Resume from "./pages/resume/Resume";
 import { Route, Routes } from "react-router-dom";
 import Contact from "./pages/contact/Contact";
+import { Fragment } from "react";
 
 // import { Routes } from "react-router-dom";
 
 function App() {
   return (
     <body>
-      <Nav/>
-        <Routes>
-           <Route path="/" element={<About />} />
-           <Route  path="/work" element={<Work/>}/>
-           <Route path="/resume" element={<Resume/>} />
-           <Route path="/contact" element={<Contact/>} />
-           
-        </Routes>
+      <Fragment></Fragment>
+      <Nav />
+      <Routes>
+        <Route path="/" element={<About />} />
+        <Route path="/work" element={<Work />} />
+        <Route path="/resume" element={<Resume />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
     </body>
   );
 }
